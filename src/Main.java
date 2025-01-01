ea2768be2ab89d3090be0b678b990963ebec8d59
RenatoAlves30 <renatodev42@gmail.com>
2025-01-01 11:30:49 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -16,15 +15,54 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
 
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//
+//        System.out.println(mentoria);
 
-        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila " + devCamila.getConteudosInscritos());
+        devCamila.progedir();
+        devCamila.progedir();
+        System.out.println("________________________________");
+        System.out.println("Conteúdos Inscritos Camila " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+        System.out.println("________________________________");
+
+        Dev devRenato = new Dev();
+        devRenato.setNome("Renato");
+        devRenato.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Renato " + devRenato.getConteudosInscritos());
+        devRenato.progedir();
+        devRenato.progedir();
+        devRenato.progedir();
+
+        System.out.println("________________________________");
+
+
+        System.out.println("Conteúdos Inscritos Renato " + devRenato.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Renato " + devRenato.getConteudosConcluidos());
+        System.out.println("XP: " + devRenato.calcularTotalXp());
+
+
 
 
     }
